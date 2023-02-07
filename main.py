ee77b32cca8c8083b4b151241796b8860ecffb60
VladFORT <vladatom0601@gmail.com>
2023-02-07 21:16:47 +0100

    experement lesson 4, lernen types
    lease enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ print(10 // 4 )
 print(min(67, 100, -98, -3))
 print(max(67, 100, -98, -3))
 print(abs(-98))
+ 
+#lesson 4
+print('lesoooooon 4')
+number = 7
+print(number)
+del number # delite number
+
+digit = 6.8342242
+word = "HELLO"
+boolean = False
+number = 2
+print(word + str(digit))
+print(word, digit, digit*10, boolean)
+str_num = '9'#string
+print(number + int(str_num))
